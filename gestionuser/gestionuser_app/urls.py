5eb90ed39f7b7edb9cfc8366f87b14962253dd18
christianemwenge <christianemwenge@gmail.com>
2025-07-07 23:04:32 +0200

    modification

--- gestionuser/gestionuser_app/urls.py
+++ gestionuser/gestionuser_app/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
 from . import views
+from .views import login_view
+from .views import logout_view
 
 urlpatterns = [
     path('', views.redirect_to_dashboard),  # redirection par défaut
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('utilisateurs/', views.utilisateur_view, name='utilisateurs'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
+    
 ]
--- gestionuser/gestionuser_app/views.py
+++ gestionuser/gestionuser_app/views.py
@@ -1,11 +1,31 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+
 def home(request):
     return render(request, 'gestionuser_app/base.html')
+@login_required(login_url='/login/')
 def dashboard_view(request):
     return render(request, 'gestionuser_app/dashboard.html')
 
+@login_required(login_url='/login/')
 def utilisateur_view(request):
     return render(request, 'gestionuser_app/utilisateur.html')
 def redirect_to_dashboard(request):
     return redirect('dashboard')  # 'dashboard' = nom donné à ta route
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('dashboard')  # ou une autre vue
+        else:
+            return render(request, 'gestionuser_app/login.html', {'error': 'Identifiants invalides'})
+    return render(request, 'gestionuser_app/login.html')
+def logout_view(request):
+    logout(request)
+    return redirect('login')
