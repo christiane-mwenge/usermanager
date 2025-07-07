4d98f5ab6a7a0170ad24192aed0ff072d218c9d0
christianemwenge <christianemwenge@gmail.com>
2025-07-07 21:44:41 +0200

    login

--- gestionuser/gestionuser/settings.py
+++ gestionuser/gestionuser/settings.py
@@ -83,8 +83,13 @@ WSGI_APPLICATION = 'gestionuser.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'user_management_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
